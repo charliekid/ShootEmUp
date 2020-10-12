13eec861e27daf1d8536b64091909d5cbcf1b9b5
Charlie Nguyen <charliekid19@gmail.com>
2020-10-11 18:27:24 -0700

    -

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public GameObject bullet;
+  
+  public Transform shottingOffset;
+  
+  public Transform player;
+
+  private float amplitude = 1;
+  
+  public float velocityPlayer = 0;
+  
+  public float maxBoundary, minBoundary;
+
+  // Update is called once per frame
+    void Update()
+    {
+      velocityPlayer = Input.GetAxis("Horizontal");
+      
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+        Debug.Log("Bang!");
+
+        Destroy(shot, 3f);
+      }
+      
+      // Moves the player within the boundaries
+      if (player.position.x < minBoundary && velocityPlayer < 0)
+      {
+        velocityPlayer = 0;
+      }
+      else if (player.position.x > maxBoundary && velocityPlayer > 0)
+      {
+        velocityPlayer = 0;
+      }
+
+      player.position += Vector3.right * velocityPlayer * amplitude;
+    }
+}
